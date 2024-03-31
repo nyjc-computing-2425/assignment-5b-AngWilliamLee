0cf26ddff6386d95d1b9890bf3efb9af944a1106
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-03-31 05:45:02 +0000

    final submission

--- main.py
+++ main.py
@@ -1,94 +1,147 @@
+# from pprint import pprint
+
 # Part 1
 def read_csv(filename):
-  """
-  1) Takes a string filename
-  2) Reads CSV data stored in filename
-  3) Returns two values, (header, data):
-  - header is a list containing the column labels (from the first row)
-  - data is a nested list containing the data.
-    Data should be converted to appropriate types, i.e. int, float, or str.
-  """
-  with open(filename, 'r') as csv_file:
-    data = []
-    for line in csv_file:
-        row_list = line.strip().split(',')
-        data.append(row_list)
-    first_row = csv_file.readline()
-    header = first_row.split(',')
-    return header, data
+    """
+    Given a filename, reads the csv file and return a list of lists
+  
+    Parameter
+    ---------
+    filename: str
+       The filename of the csv file to read
+  
+    Returns
+    -------
+    tuple
+        a tuple consisting of a header list and a nested list containing the data
+    """
+  
+    with open(filename, 'r') as csv_file:
+        first_row = csv_file.readline()
+        header = first_row.split(',')
+        data = []
+        for line in csv_file:
+            row_list = line.strip().split(',')
+            row_list[0] = int(row_list[0])
+            row_list[3] = int(row_list[3])
+            data.append(row_list)
+            
+        return header, data
+
+# pprint(read_csv("pre-u-enrolment-by-age.csv"))
+        
     
 
 # Part 2
 def filter_gender(enrolment_by_age, sex):
-  """
-  1) Takes a list of records enrolment_by_age and a string sex
-  2) Return a list of records where the value in the "sex" column matches string sex (We are only keeping data for total enrolment, not split by gender.)
-  3) Exclude the sex column from the returned records.
-  You should end up with data for three columns: year, age, and enrolment_jc.
-  """
-  filtered_records = []
-  for record in enrolment_by_age:
-      if record[-1] == sex:
-          filtered_records.append([record[0], record[1], record[2]])
-  return filtered_records
+    """
+    Given a nested list, filters by gender, returns a nested list
+
+    Parameter
+    ---------
+    enrolment_by_age: list
+      The list to be filtred
+
+    Returns
+    -------
+    filtered_records: list
+      nested list containing gender-filtered data
+    """            
+    filtered_records = []
+    for record in enrolment_by_age:
+        if record[2] == sex:
+            filtered_records.append([record[0], record[1], record[3]])
+            # filtered_records.append([record[0], record[3]])
+
+    return filtered_records
+
+# with open("pre-u-enrolment-by-age.csv", 'r') as csv_file:
+#     first_row = csv_file.readline()
+#     data = []
+#     for line in csv_file:
+#         row_list = line.strip().split(',')
+#         row_list[0] = int(row_list[0])
+#         row_list[3] = int(row_list[3])
+#         data.append(row_list)
+        
+# mf_enrolment = filter_gender(data, 'MF')
+# pprint(mf_enrolment)
 
 
 # Part 3
 def sum_by_year(enrolment_data):
-  """
-  1) Add up the total enrolment for each year, regardless of age
-  2) Return the result as a list of lists. Each inner list comprises two integers, year and total_enrolment.
-  """
-  enrolment_by_year = {} #making dictionary
-  for record in enrolment_data:
-      year = record[0]
-      enrolment = record[-1]
+    """
+    Given a nested list, sums enrolment_data by year, returns a nested list
+  
+    Parameter
+    ---------
+    enrolment_data: list
+      The list to be summed
+  
+    Returns
+    -------
+    total_enrolment : list
+      nested list with each inner list comprising year and summed data
+    """
+  
+    result = []
+
+    for record in enrolment_data:
+        year = record[0]
+        enrolment = record[2]
     
-      if year in enrolment_by_year:
-          enrolment_by_year[year] += enrolment
-      else:
-          enrolment_by_year[year] = enrolment
-  return [[year, total_enrolment] for year, total_enrolment in enrolment_by_year.items()]
+        year_found = False
+        for entry in result:
+            if entry[0] == year:
+                entry[1] += enrolment
+                year_found = True
+
+        if not year_found:
+            result.append([year, enrolment])
+    return result
+    
+# enrolment_by_year = sum_by_year(mf_enrolment)
+# pprint(enrolment_by_year)
+
 
 
 # Part 4
 def write_csv(filename, header, data):
-  """
-   write header and data to filename in CSV format and return the number of lines written. Any existing data in filename should be ignored and overwritten.
-  """
-  with open(filename, 'w') as file:
-      file.write(','.join(header) + '\n')
+    """
+    Given a filename, header and data, write them into a csv file and return csv file and number of lines in it
+  
+    Parameter
+    ---------
+    filename: str
+      The filename for the csv file to be written
+    header: list
+      The headers for the csv file to be written
+    data: list
+      The data for the csv file to be written
+  
+    Returns
+    -------
+    length_of_file: str
+      string containing the number of lines in the csv file to be written
+    """
+    with open(filename, 'w') as file:
+        file.write(','.join(header) + '\n')
 
-      for row in data:
-          row_str = ','.join(str(x) for x in row)
-          file.write(row_str + '\n')
-        
-  with open(filename, 'r') as file:
-      return len(file.readlines()) - 1
+        count = 0
+        for row in data:
+            row[0] = str(row[0])
+            row[1] = str(row[1])
+            row_str = ','.join(row)
+            file.write(row_str + '\n')
+            count += 1
+        return count
+        # return (header, data, count)
+
+# pprint(write_csv('total-enrolment-by-year.csv', ["year", "total_enrolment"], enrolment_by_year))
+
 
   
 
 # TESTING
 # You can write code below to call the above functions
 # and test the output
-# enrolment_by_age = [[1984, '17 YRS', 8710, 'girl'], [1984, '18 YRS', 3927, 'boy']]
-# print(filter_gender(enrolment_by_age, 'girl'))
-# enrolment_data = [
-#     [1984, "poo", 1000],
-#     [1984, 2000],
-#     [1985, 1500],
-#     [1985, 2000],
-#     [1985, 500]
-# ]
-# enrolment_by_year = sum_by_year(enrolment_data)
-# print(enrolment_by_year)
-# filename = 'total-enrolment-by-year.csv'
-# header = ["year", "total_enrolment"]
-# enrolment_by_year = [[2020, 1500],
-#     [2021, 1800],
-#     [2022, 2000],
-#     [2023, 2100],
-#     [2024, 2200], [2022, 1022]]
-
-# num_lines_written = write_csv(filename, header, enrolment_by_year)
-# print(num_lines_written)
